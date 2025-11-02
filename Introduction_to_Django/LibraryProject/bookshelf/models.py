a673f829fb586d51f29f1e60104f8ba0a9dfc13e
dowlib1 <oladimeji0107@gmail.com>
2025-11-02 06:36:43 -0800

    starting of the the bookshelfapp -- django

---
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class Book(models.Model):
+    """A simple book in the bookshelf."""
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
+
