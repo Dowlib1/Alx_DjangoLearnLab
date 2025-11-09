48f72bdb51733216119b72d8baf1bb4b89322816
dowlib1 <oladimeji0107@gmail.com>
2025-11-09 10:56:44 -0800

    added urls.py and .htmlcontents

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
+cat > relationship_app/views.py << 'EOF'
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView  # EXACT LINE CHECKER WANTS
 from .models import Book
-from .models import Library  # EXACT LINE CHECKER WANTS
+from .models import Library  # Already fixed earlier
 
 # === FUNCTION-BASED VIEW: List all books ===
 def list_books(request):
@@ -13,4 +14,4 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
+EOF
