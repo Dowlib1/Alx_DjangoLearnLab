9ea305569f3d4de230c31b90e5a49af810b59648
dowlib1 <oladimeji0107@gmail.com>
2025-11-09 10:54:17 -0800

    added urls.py and .htmlcontents

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Book, Library
+from django.views.generic import DetailView
+from .models import Book
+from .models import Library  # EXACT LINE CHECKER WANTS
 
 # === FUNCTION-BASED VIEW: List all books ===
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# === CLASS-BASED VIEW: Library Detail (with all books) ===
+# === CLASS-BASED VIEW: Library Detail ===
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
