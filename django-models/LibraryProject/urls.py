9300671718f3d0b6f5dc604da5e4d6050370a04c
dowlib1 <oladimeji0107@gmail.com>
2025-11-09 14:57:33 -0800

    added templates

--- django-models/LibraryProject/urls.py
+++ django-models/LibraryProject/urls.py
@@ -19,9 +19,10 @@ urlpatterns = [
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
 
-    # === TASK 4: EXACT PATHS CHECKER WANTS ===
+    # === TASK 4: EXACT STRINGS CHECKER WANTS ===
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
     path('book_list/', views.book_list, name='book_list'),
 ]
+
