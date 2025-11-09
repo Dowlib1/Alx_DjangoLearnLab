01cb97aba90ee0f9291ff1449dec296f2c740e31
dowlib1 <oladimeji0107@gmail.com>
2025-11-09 14:43:16 -0800

    added templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,18 +8,21 @@ urlpatterns = [
     # Task 1
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    # Task 2
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
-    # Task 3: Role-based URLs
+    # Task 3
     path('admin/', views.admin_view, name='admin_view'),
     path('librarian/', views.librarian_view, name='librarian_view'),
     path('member/', views.member_view, name='member_view'),
+
+    # Task 4
+    path('book/list/', views.book_list, name='book_list'),
+    path('book/add/', views.add_book, name='add_book'),
+    path('book/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
-# === TASK 4: BOOK CRUD ===
-    path('book/list/', book_list, name='book_list'),
-    path('book/add/', add_book, name='add_book'),
-    path('book/edit/<int:pk>/', edit_book, name='edit_book'),
-    path('book/delete/<int:pk>/', delete_book, name='delete_book'),
-]
+
