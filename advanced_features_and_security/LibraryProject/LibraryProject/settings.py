baf750dc9b0e29e736d9613b09804f738b60acbc
dowlib1 <oladimeji0107@gmail.com>
2025-11-16 03:54:52 -0800

    added AUTH

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -20,6 +20,7 @@ sys.path.append(str(PROJECT_ROOT))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 
 # Quick-start development settings - unsuitable for production
