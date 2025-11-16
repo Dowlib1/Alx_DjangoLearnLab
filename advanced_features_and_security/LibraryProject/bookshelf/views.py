c5496e44cc812932a2a58e81a3f1af716394744d
dowlib1 <oladimeji0107@gmail.com>
2025-11-16 05:52:50 -0800

    added auth for crud

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import permission_required, login_required
+from django.views.decorators.http import require_http_methods
+from .models import Book
+from .forms import BookForm
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    q = request.GET.get('q')
+    if q:
+        books = Book.objects.filter(title__icontains=q)
+    else:
+        books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+@require_http_methods(["GET", "POST"])
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.created_by = request.user
+            book.save()
+            return redirect('bookshelf:book_list')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+@require_http_methods(["GET", "POST"])
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect('bookshelf:book_list')
+    return render(request, 'bookshelf/form_example.html', {'form': form, 'book': book})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+@require_http_methods(["POST"])
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    book.delete()
+    return redirect('bookshelf:book_list')from django.shortcuts import render
 
 # Create your views here.
