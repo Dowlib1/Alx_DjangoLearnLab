31e40337f1b09e01e73466fdd5f62f59f1b2fe38
Dowlib1 <oladimeji0107@gmail.com>
2026-01-06 13:41:29 +0100

    add .html files in template/blog/

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('admin', admin.site.urls),
+    path('', include('blog.urls')),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('profile/', views.profile, name='profile'),
+]
