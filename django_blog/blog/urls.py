8c6c644877e581f1ed2642435721e041da90b0f3
Dowlib1 <oladimeji0107@gmail.com>
2026-01-08 00:13:50 +0100

    Add substitution post for post edit to update

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -5,21 +5,21 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path('admin', admin.site.urls),
-    path('', include('blog.urls')),
+    #path('', include('blog.urls')), This causes infinte recursionas the blog.url app contain is app itself.
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('', PostListView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post_new'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),
     #add-up
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_new'),  # But since integrated, maybe not needed
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_new'),  # But since integrated, maybe not needed
     path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
     path('search/', SearchResultsView.as_view(), name='search'),
     path('tags/<str:tag_name>/', TagView.as_view(), name='tag_posts'),
-    
+#comment or comments will fit in    
 ]
