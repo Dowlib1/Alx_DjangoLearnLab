65ac91188e4bc029f17525e2d2213bbc697c6a60
Dowlib1 <oladimeji0107@gmail.com>
2026-01-07 07:08:24 +0100

    add CRUD in url.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,19 +13,13 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('posts/new/', PostCreateView.as_view(), name='post_new'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
-    ["post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
     path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
+     path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     #add-up
     path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment_new'),  # But since integrated, maybe not needed
     path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
     path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
     path('search/', SearchResultsView.as_view(), name='search'),
-path('tags/<str:tag_name>/', TagView.as_view(), name='tag_posts'),
-    # May remove
-    path('', views.PostListView.as_view(), name='post_list'),
-    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
-    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    path('tags/<str:tag_name>/', TagView.as_view(), name='tag_posts'),
+    
 ]
