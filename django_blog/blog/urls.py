b95fed3958f41371f17f3c251001acab263afd1c
Dowlib1 <oladimeji0107@gmail.com>
2026-01-08 21:07:33 +0100

    fixed fixed typo in urls.py; str to slug for tagging

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,23 +4,21 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path('admin', admin.site.urls),
-    #path('', include('blog.urls')), This causes infinte recursionas the blog.url app contain is app itself.
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('', PostListView.as_view(), name='post_list'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('post/new/', PostCreateView.as_view(), name='post_new'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_edit'),
+    path('', view.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>/', view.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', view.PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/delete/', view.PostDeleteView.as_view(), name='post_delete'),
+     path('post/<int:pk>/update/', view.PostUpdateView.as_view(), name='post_edit'),
     #add-up
-    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_new'),  # But since integrated, maybe not needed
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment_edit'),
-     path('comment/<int:pk>/update/', CommentCreateView.as_view(), name='comment_update'), 
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
-    path('search/', SearchResultsView.as_view(), name='search'),
-    path('tag/<str:tag_name>/', TagView.as_view(), name='tag_posts'),
-#comment or comments will fit in    
+    path('post/<int:pk>/comments/new/', view.CommentCreateView.as_view(), name='comment_new'),  # But since integrated, maybe not needed
+    path('comment/<int:pk>/edit/', view.CommentUpdateView.as_view(), name='comment_edit'),
+     path('comment/<int:pk>/update/', view.CommentCreateView.as_view(), name='comment_update'), 
+    path('comment/<int:pk>/delete/', view.CommentDeleteView.as_view(), name='comment_delete'),
+    path('search/', view.SearchResultsView.as_view(), name='search'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='tag_posts'),
+#comment or comments will fit in and slug
 ]
