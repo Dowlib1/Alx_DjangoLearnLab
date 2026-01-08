41d75e790caeeb7e9f3c81de0123e78dc22c61d2
Dowlib1 <oladimeji0107@gmail.com>
2026-01-08 21:31:27 +0100

    fixed recurring Post(models.Model), import reverse

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.urls import reverse #Added for get_absolute_url
+
 
 # Create your models here.
 class Post(models.Model):
