4677081b4e3d37fed2aa0c81e7655fabf2b411d4
Dowlib1 <oladimeji0107@gmail.com>
2026-01-11 23:55:54 +0100

    Db; Postgresql added to settings.py

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import RegisterView, LoginView, ProfileView
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
@@ -12,4 +12,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', UserViewSet.as_view({'post': 'follow'}), name='user-follow'),
+    path('unfollow/<int:user_id>/', UserViewSet.as_view({'post': 'unfollow'}), name='user-unfollow'),
 ]
--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, 
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls))
+    path('<int:pk>/like/', PostViewSet.as_view({'post': 'like'}), name='post-like'),
+    path('<int:pk>/unlike/', PostViewSet.as_view({'post': 'unlike'}), name='post-unlike'),
 ]
